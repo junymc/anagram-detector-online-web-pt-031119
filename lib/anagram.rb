9d0c286da8df068b4571ca53c7e4728158f45416
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-06 01:33:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
 
   def match(words_array)
     words_array.select do |word|
-      @word.split("") == word.split("")
+      @word.split("").sort == word.split("").sort
     end
   end
 
