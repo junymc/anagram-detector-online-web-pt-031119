5cd4005ca7e4171ba18541a92338ff66e7d75d78
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-06 01:37:25 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,8 @@
 require 'pry'
 class Anagram
-
+  # Anagram is a word, phrase, or sentence formed from another by rearranging its letters
+  # ex)“Angel” is an anagram of “glean.”
+  
   attr_accessor :word
 
   def initialize(word)
@@ -10,6 +12,7 @@ class Anagram
   def match(words_array)
     words_array.select do |word|
       @word.split("").sort == word.split("").sort
+      # If the letters are not in the matching oeder, it returns false so it will return an empty array
     end
   end
 
